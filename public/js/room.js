6c331ea13252b35457c18eb8d3488877ed511205
Amey Gupta <ameygupta9211@gmail.com>
2021-06-27 23:32:14 +0530

    UI changes

--- public/js/room.js
+++ public/js/room.js
@@ -11,6 +11,7 @@ const continueButt = document.querySelector('.continue-name');
 const nameField = document.querySelector('#name-field');
 const videoButt = document.querySelector('.novideo');
 const audioButt = document.querySelector('.audio');
+const inviteParticipants = document.querySelector('.invite');
 const cutCall = document.querySelector('.cutcall');
 const screenShareButt = document.querySelector('.screenshare');
 const whiteboardButt = document.querySelector('.board-icon')
@@ -18,130 +19,6 @@ const whiteboardButt = document.querySelector('.board-icon')
 const leftcont = document.querySelector('.left-cont')
 const rightcont = document.querySelector('.right-cont')
 const chatbutton = document.querySelector('.showchat')
-
-// whiteboard js start
-
-// const whiteboardCont = document.querySelector('.whiteboard-cont');
-// const canvas = document.querySelector("#whiteboard");
-// const ctx = canvas.getContext('2d');
-
-// let boardVisisble = false;
-
-// whiteboardCont.style.visibility = 'hidden';
-
-// let isDrawing = 0;
-// let x = 0;
-// let y = 0;
-// let color = "black";
-// let drawsize = 3;
-// let colorRemote = "black";
-// let drawsizeRemote = 3;
-
-// function fitToContainer(canvas) {
-//     canvas.style.width = '100%';
-//     canvas.style.height = '100%';
-//     canvas.width = canvas.offsetWidth;
-//     canvas.height = canvas.offsetHeight;
-// }
-
-// fitToContainer(canvas);
-
-// //getCanvas call is under join room call
-// socket.on('getCanvas', url => {
-//     let img = new Image();
-//     img.onload = start;
-//     img.src = url;
-
-//     function start() {
-//         ctx.drawImage(img, 0, 0);
-//     }
-
-//     console.log('got canvas', url)
-// })
-
-// function setColor(newcolor) {
-//     color = newcolor;
-//     drawsize = 3;
-// }
-
-// function setEraser() {
-//     color = "white";
-//     drawsize = 10;
-// }
-
-// //might remove this
-// function reportWindowSize() {
-//     fitToContainer(canvas);
-// }
-
-// window.onresize = reportWindowSize;
-// //
-
-// function clearBoard() {
-//     if (window.confirm('Are you sure you want to clear board? This cannot be undone')) {
-//         ctx.clearRect(0, 0, canvas.width, canvas.height);
-//         socket.emit('store canvas', canvas.toDataURL());
-//         socket.emit('clearBoard');
-//     }
-//     else return;
-// }
-
-// socket.on('clearBoard', () => {
-//     ctx.clearRect(0, 0, canvas.width, canvas.height);
-// })
-
-// function draw(newx, newy, oldx, oldy) {
-//     ctx.strokeStyle = color;
-//     ctx.lineWidth = drawsize;
-//     ctx.beginPath();
-//     ctx.moveTo(oldx, oldy);
-//     ctx.lineTo(newx, newy);
-//     ctx.stroke();
-//     ctx.closePath();
-
-//     socket.emit('store canvas', canvas.toDataURL());
-
-// }
-
-// function drawRemote(newx, newy, oldx, oldy) {
-//     ctx.strokeStyle = colorRemote;
-//     ctx.lineWidth = drawsizeRemote;
-//     ctx.beginPath();
-//     ctx.moveTo(oldx, oldy);
-//     ctx.lineTo(newx, newy);
-//     ctx.stroke();
-//     ctx.closePath();
-
-// }
-
-// canvas.addEventListener('mousedown', e => {
-//     x = e.offsetX;
-//     y = e.offsetY;
-//     isDrawing = 1;
-// })
-
-// canvas.addEventListener('mousemove', e => {
-//     if (isDrawing) {
-//         draw(e.offsetX, e.offsetY, x, y);
-//         socket.emit('draw', e.offsetX, e.offsetY, x, y, color, drawsize);
-//         x = e.offsetX;
-//         y = e.offsetY;
-//     }
-// })
-
-// window.addEventListener('mouseup', e => {
-//     if (isDrawing) {
-//         isDrawing = 0;
-//     }
-// })
-
-// socket.on('draw', (newX, newY, prevX, prevY, color, size) => {
-//     colorRemote = color;
-//     drawsizeRemote = size;
-//     drawRemote(newX, newY, prevX, prevY);
-// })
-
-// whiteboard js end
 
 let videoAllowed = 1;
 let audioAllowed = 1;
@@ -244,12 +121,10 @@ function handleGetUserMediaError(e) {
 
 }
 
-
 function reportError(e) {
     console.log(e);
     return;
 }
-
 
 function startCall() {
 
@@ -412,7 +287,6 @@ socket.on('video-offer', handleVideoOffer);
 socket.on('new icecandidate', handleNewIceCandidate);
 
 socket.on('video-answer', handleVideoAnswer);
-
 
 socket.on('join room', async (conc, cnames, micinfo, videoinfo) => {
     socket.emit('getCanvas');
@@ -655,6 +529,10 @@ audioButt.addEventListener('click', () => {
 
         socket.emit('action', 'unmute');
     }
+})
+
+inviteParticipants.addEventListener('click', () => {
+    
 })
 
 socket.on('action', (msg, sid) => {
